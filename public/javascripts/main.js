2e9cff436fee6b61076ad553512dbf88e066381e
LiyLi1122 <teppei.252033@gmail.com>
2022-06-14 10:44:54 +0800

    add an event on main.js

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,5 +3,6 @@ const input = document.querySelector('input')
 form.addEventListener('submit', (event) => {
   if (!input.value.trim()) {
     event.preventDefault()
+    event.stopPropagation()
   }
 })
